8e58b7880c4b3b9a16b3b2ad64615a26cf17836d
elokac <105647459+elokac@users.noreply.github.com>
2022-06-26 21:06:58 +0100

    Update privileges.sh

--- scripts/privileges.sh
+++ scripts/privileges.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+chmod +x /home/ec2-user/SeamFixServer/server_start.sh
+chmod +x /home/ec2-user/SeamFixServer/server_stop.sh
